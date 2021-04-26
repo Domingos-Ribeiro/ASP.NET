8288153d6aa363ec98b55268450252e877e8ba5c
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-04-26 16:02:18 +0100

    C

---
+++ Controllers/CController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EquipasMembros.DAL;
+using EquipasMembros.Models;
+
+namespace EquipasMembros.Controllers
+{
+    public class CController : Controller
+    {
+        private EquipasMembrosContext dbPointer = new EquipasMembrosContext();
+        // GET: C
+        public ActionResult Index()
+        {
+            // Preparação da drop 1 com as Equipas:
+            List<Equipas> listaDasEquipas = new List<Equipa>();
+            listaDasEquipas = dbPointer.Equipas.ToList();
+
+            return View();
+        }
+    }
+}
