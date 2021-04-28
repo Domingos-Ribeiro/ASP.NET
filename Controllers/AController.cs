945770de9c34e133a63a00ad62c5ae076c86973f
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-04-28 07:53:21 +0100

    abcde comlpleto

---
+++ Controllers/AController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EquipasMembros.DAL;
+using EquipasMembros.Models;
+
+namespace EquipasMembros.Controllers
+{
+    public class AController : Controller
+    {
+        // GET: A
+        public ActionResult Index()
+        {
+            // Declarar a lista "Manual" para conter as strings:
+            List<string> listaDeNomes = new List<string>(); // Sintaxe original
+            listaDeNomes.Add("Anabela");
+            listaDeNomes.Add("Joaquim");
+
+            // Enviar lista criada em cima para a view
+            ViewBag.LISTA = listaDeNomes;
+
+
+            // Declarar lista "Manual" para conter as strings e aparecer na dropdown
+            var listaDeClubes = new List<string>(); // Sintaxe Alternativa
+
+            // Iniciar a lista:
+            listaDeClubes.Add("Benfica");
+            listaDeClubes.Add("Porto");
+            listaDeClubes.Add("Braga");
+            listaDeClubes.Add("Gilvicente");
+
+            listaDeClubes.Sort(); // Ordenar a lista de clubes
+
+            // Criar uma selectlist ou Dropdown com os elementos da lista:
+            var sl = new SelectList(listaDeClubes);
+
+            // Enviar lista para a view
+            ViewBag.SELECTLIST = sl;
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/DController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EquipasMembros.DAL;
+using EquipasMembros.Models;
+
+namespace EquipasMembros.Controllers
+{
+    public class DController : Controller
+    {
+        private EquipasMembrosContext dbpointer = new EquipasMembrosContext();
+
+        // GET: D
+        public ActionResult Index(string codigoClicado)
+        {
+            //Levar as Equipas sem recorrer ao model na view
+            List<Equipa> list = new List<Equipa>();
+            list = dbpointer.Equipas.ToList();
+            SelectList sel = new SelectList(list, "Id", "NomeEquipa");
+            ViewBag.SEL = sel;
+
+            if (String.IsNullOrEmpty(codigoClicado)) // Primeira vez
+            {
+                
+                codigoClicado = "Clique no botão em cima para ver o código";
+            }
+
+            // Enviar o codigo da Equipa para a view
+            ViewBag.CODIGOCLICADO = codigoClicado;
+
+            return View();
+        }
+    }
+}
