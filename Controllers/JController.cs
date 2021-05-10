61c5e15bb60677ea1d076706ea144ef5727e435e
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-10 12:02:41 +0100

    J

--- Controllers/JController.cs
+++ Controllers/JController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EquipasMembros.DAL;
+using EquipasMembros.Models;
 
 namespace EquipasMembros.Controllers
 {
     public class JController : Controller
     {
+        private EquipasMembrosContext pointer = new EquipasMembrosContext();
         // GET: J
-        public ActionResult Index()
+        public ActionResult Index(int? idDrop)
         {
-            return View();
+            //  Buscar a lista das equipas para enviar para a dropbox
+            List<Equipa> lista = pointer.Equipas.ToList();
+            SelectList sel = new SelectList(lista, "Id", "NomeEquipa");
+            ViewBag.LISTA = sel;
+
+            // Parametro idDrop tem o id (ou chave primária) da equipa escolhida
+            // então, filtrar os membros em que a chave primária = idDrop
+            var membros = pointer.Membros.Where(m => m.EquipaId == idDrop);
+
+
+            //var membros = pointer...
+            return View(membros.ToList());
+
+
         }
     }
 }
---
+++ Controllers/RRController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EquipasMembros.Controllers
+{
+    public class RRController : Controller
+    {
+        // GET: RR
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
