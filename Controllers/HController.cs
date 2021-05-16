d366a8e69401d79bb0955ce158ad7f62f4a682b1
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-17 00:05:55 +0100

    H

---
+++ Controllers/HController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EquipasMembros.Controllers
+{
+    public class HController : Controller
+    {
+        // GET: H
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
