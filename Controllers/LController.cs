d9d7da73e7da200f6cca9e1314571531a38b09f5
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-18 14:37:51 +0100

    S4

---
+++ Controllers/LController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EquipasMembros.Controllers
+{
+    public class LController : Controller
+    {
+        // GET: L
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
