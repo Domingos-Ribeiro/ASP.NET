571feb163a76a6eaedd979ab61998f7abb6b6273
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-21 02:38:50 +0100

    S3

---
+++ Controllers/S3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EquipasMembros.Controllers
+{
+    public class S3Controller : Controller
+    {
+        // GET: S3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
