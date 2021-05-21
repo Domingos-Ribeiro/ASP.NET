6cc358a33fb31824bd0e999ef0b6f7dd18c51c51
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-21 02:40:11 +0100

    S4

---
+++ Controllers/S4Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EquipasMembros.Controllers
+{
+    public class S4Controller : Controller
+    {
+        // GET: S4
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
