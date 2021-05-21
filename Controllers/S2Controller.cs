312f11bdafcfdaa4a110c94ed9bd80e0cb476e2c
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-21 02:37:36 +0100

    S2

---
+++ Controllers/S2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EquipasMembros.Controllers
+{
+    public class S2Controller : Controller
+    {
+        // GET: S2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
