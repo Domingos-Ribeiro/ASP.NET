94c15b81d079a5567258f49f0fa3b9dce62b5f20
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-05-26 00:55:04 +0100

    H 1

--- Controllers/HController.cs
+++ Controllers/HController.cs
@@ -1,17 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EquipasMembros.DAL;
+using EquipasMembros.Models;
+using EquipasMembros.ViewModel;
 
 namespace EquipasMembros.Controllers
 {
     public class HController : Controller
     {
         // GET: H
-        public ActionResult Index()
+        private EquipasMembrosContext db = new EquipasMembrosContext();
+        
+        public ActionResult Index(int? id)
         {
-            return View();
+            //A ViewModel poderá aceder a qualquer um dos atributos da Class EquipaMembros.
+            // São eles: Equipa e Membros (Ver a pasta ViewModel)
+            var viewModel = new EquipaMembros();
+
+            // Leva as Equipas para a View:
+            viewModel.Equipas = db.Equipas.Include(i => i.Membros.Select(c => c.Equipa));
+
+            // Leva os Membros para a View:
+            viewModel.Membros = db.Membros;
+
+            // Recebe a Equipa clicada via argumento e devolve á View:
+            ViewBag.equipaclicada = id;
+
+            ViewBag.TotalDeMembros = db.Membros.Count();
+
+            if (id == -1)
+            {
+                ViewBag.TotalDeMembrosDaEquipa = "";
+            }
+            else
+            {
+                
+                {
+                    int totequipa = db.Membros.Where((i => i.EquipaId == id)).Count();
+                      if (totequipa > 0)
+                         ViewBag.TotalDeMembrosDaEquipa = totequipa;
+                
+                        else
+                
+                         ViewBag.TotalDeMembrosDaEquipa = "0";
+                }
+
+            //---------------------------------------------------------------------------
+            // Registo terá a estrutura do modelo Equipa
+            // Só haverá um registo com aquele id:
+
+            Equipa registo;
+
+            if (id!=-1)
+            {
+                    registo = db.Equipas.Find(id);
+                    ViewBag.EquipaSelecionada = registo.NomeEquipa.ToString();
+                    ViewBag.NumeroDaEquipaSelecionada = registo.Id;
+            }
+
+            else
+                {
+
+                }
+            }
+
+
+
+            return View(viewModel);
         }
     }
 }
